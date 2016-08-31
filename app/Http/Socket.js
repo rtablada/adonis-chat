0c41292e4529c3ffe0575d1d2b7aa583cdf3b850
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-31 10:13:30 -0500

    wip: Leave chat

--- app/Http/Socket.js
+++ app/Http/Socket.js
@@ -6,12 +6,13 @@ module.exports = (server) => {
   const io = use('socket.io')(server);
 
   io.on('connection', (socket) => {
+    const rooms = {};
     // Listens for users to join a channel
     socket.on('join', (roomName) => {
       console.log('user joined: ' + roomName);
 
       // Listens for local event called "post.create"
-      Event.on('post.create', function * (post) {
+      rooms[roomName] = Event.on('post.create', function * (post) {
         console.log('post: ' + post.room);
 
         // Checks if the post is in the current room
@@ -21,6 +22,11 @@ module.exports = (server) => {
             .emit('post', post.toJSON());
         }
       });
+    });
+
+    // I think this leaves chat.
+    socket.on('leave', (roomName) => {
+      Event.removeListeners(rooms[roomName]);
     });
 
     console.log('connection created >>>');
